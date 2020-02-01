9a11a4ffa21e121c5eee28b7ebcda8fce091cd66
SDellaoui <s.dellaoui@gmail.com>
2020-02-01 15:10:59 +0100

    Add Dart Launcher

---
+++ Assets/Scripts/GameIngredients/Dart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dart : MonoBehaviour
+{
+    public float _speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.up * _speed * Time.deltaTime;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
