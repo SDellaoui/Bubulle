a534bc5b7987c6063f1291067dd5f95214dbf213
Daniel Mevoroth Quach <daniel@quach.info>
2020-02-02 01:45:40 +0100

    Integrate string system + remove offset

--- Assets/Scripts/Ball/BallStringColliderBehaviour.cs
+++ Assets/Scripts/Ball/BallStringColliderBehaviour.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class BallStringColliderBehaviour : MonoBehaviour
 {
-	public Vector3 Offset = Vector3.zero;
 	public float Radius = 1.0f;
 	public float RepulsionStrength = 10.0f;
 
 	void OnDrawGizmosSelected()
 	{
-		Gizmos.DrawSphere(transform.position + Offset, Radius);
+		Gizmos.DrawSphere(transform.position, Radius);
 	}
 
 	public bool IsInside(Vector3 Position)
 	{
-		Vector3 ColliderPosition = transform.position + Offset;
+		Vector3 ColliderPosition = transform.position;
 		Vector3 PositionToColliderPosition = ColliderPosition - Position;
 		return PositionToColliderPosition.sqrMagnitude < Radius * Radius;
 	}
 
 	public Vector3 GetRepulsionForce(Vector3 Position)
 	{
-		Vector3 ColliderPosition = transform.position + Offset;
+		Vector3 ColliderPosition = transform.position;
 		return (Position - ColliderPosition).normalized * RepulsionStrength;
 	}
 }
