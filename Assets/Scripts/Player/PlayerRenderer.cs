449b3c7a65bcb1ca66714f623ee6ce84d8ba1e4d
SDellaoui <s.dellaoui@gmail.com>
2020-02-02 18:00:31 +0100

    Add Reverb on SFX + Prepare renderer Script for player

---
+++ Assets/Scripts/Player/PlayerRenderer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRenderer : MonoBehaviour
+{
+    private Rigidbody2D m_rb;
+    private float m_speed;
+    public GameObject _renderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        m_speed = GetComponent<PlayerController>()._speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (m_rb.velocity.magnitude > 0.1f)
+        {
+            float speed = Mathf.Clamp(m_rb.velocity.magnitude, 0f, m_speed);
+            _renderer.transform.localPosition = Vector3.zero;
+            return;
+        }*/
+
+        float speed = Mathf.Clamp(m_rb.velocity.magnitude, 0f, 13);
+        speed = 1 - (m_rb.velocity.magnitude / 13);
+        //Debug.Log($"CurrentSpeed : {speed}");
+        //_renderer.transform.localPosition = new Vector3(0f, Mathf.Sin(Time.time * 2) * speed, 0.0f);
+
+    }
+}
